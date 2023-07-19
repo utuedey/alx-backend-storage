a8525700bc507aab370cac368c715a4532ad1bb5
utuedey <utuedeyejosph@gmail.com>
2023-07-18 20:48:21 -0700

    add task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+9-insert_school module
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
