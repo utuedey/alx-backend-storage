68c3226f26d4f9cd581299c84aac335ee26045c7
utuedey <utuedeyejosph@gmail.com>
2023-07-18 20:57:44 -0700

    add task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+10-update_topics module
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    mango_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
