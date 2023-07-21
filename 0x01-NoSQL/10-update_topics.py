ab86db3a0b8542a27f7466f608b156095cd792bc
utuedey <utuedeyejosph@gmail.com>
2023-07-21 10:05:27 -0700

    rename mango to mongo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mango_collection.update_many(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
